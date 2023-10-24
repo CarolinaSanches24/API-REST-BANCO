4d68e6f76cc1c51078ed8e4cb64bbbe1e227e96f
CarolinaSanches24 <carolmosilva34@gmail.com>
2023-10-24 19:50:21 -0300

    Implementacao da biblioteca dotenv protecao de dados

--- src/intermediarios.js
+++ src/intermediarios.js
@@ -1,53 +1,54 @@
-
 const validarSenha = (req, res, next) => {
-    const { senha_banco } = req.query;
+  const { senha_banco } = req.query;
+  const senha = process.env.SENHA;
+  if (senha_banco !== senha) {
+    return res
+      .status(401)
+      .json({ mensagem: "A senha do banco informada é inválida!" });
+  }
 
-    if (senha_banco !== "Bank1234") {
-        return res.status(401).json({ mensagem: "A senha do banco informada é inválida!" });
-    }
-
-
-    next();
-}
+  next();
+};
 const TodosOsCamposPreenchidosParaCriarConta = (req, res, next) => {
-    const { nome, cpf, data_nascimento, telefone, email, senha } = req.body;
+  const { nome, cpf, data_nascimento, telefone, email, senha } = req.body;
 
-    if (!nome || !cpf || !data_nascimento || !telefone || !email || !senha) {
-        return res.status(400).json({ mensagem: "Todos os campos são obrigátorios" });
-    }
+  if (!nome || !cpf || !data_nascimento || !telefone || !email || !senha) {
+    return res
+      .status(400)
+      .json({ mensagem: "Todos os campos são obrigátorios" });
+  }
 
-    next();
-}
+  next();
+};
 const TodosOsCamposPreenchidosParaAtualizarConta = (req, res, next) => {
-    const { nome, cpf, data_nascimento, telefone, email, senha } = req.body;
+  const { nome, cpf, data_nascimento, telefone, email, senha } = req.body;
 
-    if (!nome || !cpf || !data_nascimento || !telefone || !email || !senha) {
-        return res.status(400).json({ mensagem: "Todos os campos são obrigátorios" });
-    }
-    next();
-}
+  if (!nome || !cpf || !data_nascimento || !telefone || !email || !senha) {
+    return res
+      .status(400)
+      .json({ mensagem: "Todos os campos são obrigátorios" });
+  }
+  next();
+};
 const verificarNumeroContaSenhaForamInformados = (req, res, next) => {
-    const { numero_conta, senha } = req.query;
+  const { numero_conta, senha } = req.query;
 
-    if (!numero_conta) {
-        return res.status(400).json({
-            mensagem: "O preenchimento do Numero da Conta" +
-                " é obrigátorio"
-        });
-    }
-    if (!senha) {
-        return res.status(400).json({
-            mensagem: "O Prechimento da senha" +
-                " é obrigátorio"
-        });
-    }
-    next();
-}
+  if (!numero_conta) {
+    return res.status(400).json({
+      mensagem: "O preenchimento do Numero da Conta" + " é obrigátorio",
+    });
+  }
+  if (!senha) {
+    return res.status(400).json({
+      mensagem: "O Prechimento da senha" + " é obrigátorio",
+    });
+  }
+  next();
+};
 
 module.exports = {
-    validarSenha,
-    verificarNumeroContaSenhaForamInformados,
-    TodosOsCamposPreenchidosParaCriarConta,
-    TodosOsCamposPreenchidosParaAtualizarConta,
-
-}
+  validarSenha,
+  verificarNumeroContaSenhaForamInformados,
+  TodosOsCamposPreenchidosParaCriarConta,
+  TodosOsCamposPreenchidosParaAtualizarConta,
+};
